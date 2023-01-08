93678eae34171ec2e7cb3395bfa9b367f69f8a8c
Tommaso Paoli <tomaoli7612@gmail.com>
2023-01-08 13:21:27 +0100

    quasi

--- crawler.py
+++ crawler.py
@@ -7,30 +7,27 @@ def canzone(url, nomeCanzone, nomeArtista):
 
     div = soup.find('div', {'class': 'chiave'})
     try:
-        with  open('canzoni/' + nomeArtista.strip() + "." + nomeCanzone.strip() + ".txt", 'w', encoding="utf-8") as f:
+        with open('canzoni/' + nomeArtista.strip() + "." + nomeCanzone.strip() + ".canzone.txt", 'w', encoding="utf-8") as f:
+            for r in div.text.split('\n'):
+                f.write(r)
+        with open('testi/' + nomeArtista.strip() + "." + nomeCanzone.strip() + ".testo.txt", 'w', encoding="utf-8") as f:
             print(nomeCanzone.strip())
             for r in div.text.split('\n'):
                 r = r.strip()
-                if not r:
+                if (not r) or (any(c in r for c in ['DO', 'RE', 'MI', 'FA', 'SOL', 'LA', 'SI', 'Intro', '---'])):
                     continue
-
-                chords = False
-                for c in ['DO', 'RE', 'MI', 'FA', 'SOL', 'LA', 'SI']:
-                    if c in r:
-                        chords = True
-                if (chords):
-                    continue
-
                 f.write(r + '\n')
     except:
         pass
-    
+
+
 def artista(url, nomeArtista):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser', from_encoding="utf-8")
     for div in soup.find_all('div', {'class': 'archives'}):
         nomeCanzone = div.text
         canzone(div.find('a')['href'], nomeCanzone, nomeArtista)
+
 
 response = requests.get('https://www.accordiespartiti.it/accordi-chitarra/')
 soup = BeautifulSoup(response.text, 'html.parser', from_encoding="utf-8")
